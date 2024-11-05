8d20f36eddb3e07f2e6247181bf3283b4153141d
Mert KELLECİ <mertkelleci99@gmail.com>
2024-11-05 15:07:41 +0300

    Timing in C++

--- TimingInCPP/TimingInCPP.cpp
+++ TimingInCPP/TimingInCPP.cpp
@@ -2,6 +2,30 @@
 #include <thread>
 #include <chrono>
 
+struct Timer
+{
+    std::chrono::time_point<std::chrono::_V2::system_clock> start, end;
+    std::chrono::duration<float> duration;
+
+    Timer() { start = std::chrono::high_resolution_clock::now(); }
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        float ms = duration.count() * 1000.0f;
+        std::cout << "Timer took: " << ms << "ms..." << std::endl;
+    }
+};
+
+void Function()
+{
+    Timer timer;
+    for (int i = 0; i < 50; i++)
+    {
+        std::cout << "Hello World!...\n";
+    }
+}
+
 int main()
 {
     using namespace std::literals::chrono_literals;
@@ -11,5 +35,6 @@ int main()
 
     std::chrono::duration<float> duration = end - start;
     std::cout << duration.count() << "s" << std::endl;
+    Function();
     std::cin.get();
 }
