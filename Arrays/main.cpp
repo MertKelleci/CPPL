dec1728c4797cee274bf19a89d17d0b3a4b5ce41
MertKelleci <mertkelleci99@gmail.com>
2026-03-24 17:48:52 +0300

    Strings further explained

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+class Entity
+{
+public:
+    //Since declaring an array in this way creates it in the stack, the array itself will be embeded inside the memory block assigned for this Entity object.
+    //int example[5];
+
+	//Since declaring an array in this way creates it in the heap, the array will be allocated separately from the Entity object, and the pointer to the array will be stored inside the Entity object. This allows for more flexible memory management, but it also means that we need to manually manage the memory for the array, including deleting it when we're done using it to avoid memory leaks.
+    //This is whats called memory fragmentation.
+	int* example = new int[5];
+
+    Entity()
+    {
+        int a[5];
+		int* b = new int[5];
+
+		//Passing an array created in the stack to the 'sizeof' function will give us the total size of the array in bytes, which is 20 bytes for an array of 5 integers (assuming each integer is 4 bytes). However, passing a pointer to an array created in the heap to the 'sizeof' function will give us the size of the pointer itself, which is typically 8 bytes on a 64-bit system, regardless of the size of the array it points to. This is because 'sizeof' only knows about the size of the pointer, not the size of the memory it points to.
+		int count = sizeof(a) / sizeof(int);
+		int count2 = sizeof(b) / sizeof(int);
+
+        for (int i = 0; i < 5; i++)
+            example[i] = 2;
+	}
+};
+
 int main()
 {
     // This array's lifetime ends when we hit the endbracket since it's created in stack.
@@ -8,7 +32,7 @@ int main()
         example[i] = 5 - i;
 
 	//Since arrays are contiguous blocks of memory and simply an int pointer, we can use pointer arithmetic to access and modify their elements.
-    int *ptr = example;
+    int* ptr = example;
     example[1] = 5;
 
 	//Since ptr is an int pointer, adding 2 to it will move the pointer to the next integer in the array, which is example[2]. So, *(ptr + 2) is equivalent to example[2].
@@ -29,8 +53,9 @@ int main()
     }
     std::cout << std::endl;
 
-    // This array is created on heap so it must be deleted manually.
-    int *another = new int[5];
+	// This array is created on heap so it must be deleted manually. While this approach gives us more control over the lifetime of the array, it can also cause problems such as memory leaks if we forget to delete the array when we're done using it or memory fragmentation if we create and delete many arrays of different sizes over time.
+    //Furhter detail on memory fragmentation is on Entity class.
+    int* another = new int[5];
     delete[] another;
     std::cin.get();
 }
--- Strings/main.cpp
+++ Strings/main.cpp
@@ -9,8 +9,8 @@ void PrintString(const std::string &string)
 int main()
 {
     // Characters are defined with singlue quote, if double quote used its automatically defaults to char pointer
-    const char *name = "Merdo";
-    // This will keep printing until it comes across a byte with 00. You can manually add to stop with either 0 or '\0'
+    const char* name = "Merdo";
+    // The following isn't actually a char pointer, but a char array. If you were to print this without the 0 at the end, it would keep printing until it comes across a byte with 00. You can manually add to stop with either 0 or '\0'
     char name2[7] = {'C', 'h', 'e', 'r', 'n', 'o', 0};
 
     // To append strings, either:
@@ -19,8 +19,11 @@ int main()
     // OR
     // std::string name3 = std::string("something") + "something else"
     std::string name3 = "Cherno";
-
+    
+	//std::string::npos is a constant that represents the maximum value for size_t, which is typically used to indicate that a substring was not found in a string.When you call the find() method on a string, it returns the position of the first occurrence of the specified substring.If the substring is not found, it returns std::string::npos.
     bool contains = name3.find("no") != std::string::npos;
+
+	// Since PrintString method doesn't change the string, we can pass it by reference to avoid unnecessary copying of the string. This is more efficient, especially for larger strings.
     std::cout << &name3 << std::endl;
     PrintString(name3);
     std::cin.get();
